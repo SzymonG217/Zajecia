ebed169ec6c3cba169331b0d42c945bd2aecdabd
Lechh <6462503-Lechh@users.noreply.replit.com>
2024-03-20 13:17:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    String message = "Hello World";
+    for (int i = 0; i < message.length(); i++)
+    System.out.println(message.charAt(i));
+  }
+
+}
